027d2090af07b91b995b3d93fa056f884703f425
Santosh Ugale <santoshugale1991@gmail.com>
2024-09-17 12:54:40 +0530

    Refactored routing

--- angulardotnetcorefullstackwebapplication.client/src/app/app-routing.module.ts
+++ angulardotnetcorefullstackwebapplication.client/src/app/app-routing.module.ts
@@ -5,16 +5,36 @@ import { PageNotFoundComponent } from './components/page-not-found/page-not-foun
 
 const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'transactions',
+    pathMatch : 'full'
+  },
+  {
     path: 'error',
     component: ErrorComponent,
     title: 'Error'
   },
-  { path: 'transactions', loadChildren: () => import('./modules/transactions/transactions.module').then(m => m.TransactionsModule) },
-  { path: 'positions', loadChildren: () => import('./modules/positions/positions.module').then(m => m.PositionsModule) },
-  { path: '**', component: PageNotFoundComponent }
+  {
+    path: 'transactions',
+    loadChildren: () => import('./modules/transactions/transactions.module').then(m => m.TransactionsModule)
+  },
+  {
+    path: 'positions',
+    loadChildren: () => import('./modules/positions/positions.module').then(m => m.PositionsModule)
+  },
+  {
+    path: 'page-not-found',
+    title: 'PageNotFound',
+    component: PageNotFoundComponent,
+  },
+  {
+    path: '**',
+    title: 'PageNotFound',
+    redirectTo: 'page-not-found'
+  }
 ];
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, /*{ useHash: true }*/)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
