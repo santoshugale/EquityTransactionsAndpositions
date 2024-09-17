acc4c4ca129c3fc58062731b16dd6fe04a55d281
Santosh Ugale <santoshugale1991@gmail.com>
2024-09-17 15:55:54 +0530

    Added header and footer

--- angulardotnetcorefullstackwebapplication.client/src/app/app.module.ts
+++ angulardotnetcorefullstackwebapplication.client/src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ErrorComponent } from './components/error/error.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ErrorComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
